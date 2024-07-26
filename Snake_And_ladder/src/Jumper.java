ef44802eaabd7102011e852513e98638af2275dc
Narayani Anil Wanve <narayaniwanve11@gmail.com>
2024-07-26 16:51:43 +0530

    Add files via upload

---
+++ Snake_And_ladder/src/Jumper.java
@@ -0,0 +1,16 @@
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+public class Jumper {
+    int startPoint;
+    int endPoint;
+
+    public Jumper(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
---
+++ Snake_And_ladder/src/Player.java
@@ -0,0 +1,29 @@
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode
+class Player {
+    private String playerName;
+    private int id;
+
+    Player(String playerName, int id) {
+        this.playerName = playerName;
+        this.id = id;
+        
+    }
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
